9372dc531f745c126d43af39441a647a580bf3be
ReyU-sys <76889574+ReyU-sys@users.noreply.github.com>
2021-05-21 00:53:56 +0700

    update view

--- main.js
+++ main.js
@@ -24,3 +24,16 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+$(document).ready(function(){
+    function clock() {
+      var now = new Date();
+      var secs = ('0' + now.getSeconds()).slice(-2);
+      var mins = ('0' + now.getMinutes()).slice(-2);
+      var hr = now.getHours();
+      var Time = hr + ":" + mins + ":" + secs;
+      document.getElementById("watch").innerHTML = Time;
+      requestAnimationFrame(clock);
+    }
+
+    requestAnimationFrame(clock);
+});
